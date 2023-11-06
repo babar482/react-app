a483524225b428bd522125c2aa04468455b5fb39
babar482 <babarali.bcsf18@iba-suk.edu.pk>
2023-11-06 12:35:08 +0500

    2nd commit

---
+++ src/components/HeroSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HeroSection() {
+  return (
+    <section className="hero">
+      <h2>Welcome to My App</h2>
+      <p>This is the hero section of the app.</p>
+    </section>
+  );
+}
+
+export default HeroSection;
